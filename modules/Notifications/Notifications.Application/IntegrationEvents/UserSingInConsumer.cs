cefcbe510bc27a667dfcd3f66933903bcec0dc92
Fabian Ramirez <fabian.ramirez@onoff.com.co>
2025-05-19 07:46:19 -0500

    Add consumers notificatiosn

---
+++ modules/Notifications/Notifications.Application/IntegrationEvents/UserSingInConsumer.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Events;
+using MassTransit;
+using Notifications.Application.Abstractions.Emails;
+using Notifications.Domain.Modules.Entities;
+
+namespace Notifications.Application.IntegrationEvents;
+
+public class UserSingInConsumer(IEmailService emailServices) : IConsumer<UserSignInEvent>
+{
+    public async Task Consume(ConsumeContext<UserSignInEvent> context)
+    {
+        await emailServices.SendEmail(new Email
+        {
+            Subject = "Nuevo Registro Realizado",
+            Body = "Bienvenido A la Aplicacion, Esperamos que sea de mucho entuciasmo para ti.",
+            To = context.Message.Email
+        });
+
+    }
+}
