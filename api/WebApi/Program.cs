62e30745285230fae951fd2932bad84ab5703cb8
Fabian Ramirez <fabian.ramirez@onoff.com.co>
2025-05-19 07:46:37 -0500

    Settigns producers and consumers

--- api/WebApi/Program.cs
+++ api/WebApi/Program.cs
@@ -2,7 +2,9 @@ using Application;
 using Authentications.Application;
 using Authentications.Infrastructure;
 using Infrastructure;
+using MassTransit;
 using Notifications.Application;
+using Notifications.Application.IntegrationEvents;
 using Notifications.Infrastructure;
 using WebApi;
 
@@ -12,10 +14,29 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 IServiceCollection services = builder.Services;
 IConfigurationManager configuration = builder.Configuration;
 
+
+services.AddMassTransit(x =>
+{
+    // Registrar los consumidores para los eventos que escucharán
+    x.AddConsumer<UserSingUpConsumer>();
+    x.AddConsumer<UserSingInConsumer>();
+
+    // Configurar RabbitMQ
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host("rabbitmq://localhost");  
+
+        cfg.ReceiveEndpoint("user-signup-queue", e => e.ConfigureConsumer<UserSingUpConsumer>(context));
+
+        cfg.ReceiveEndpoint("user-signin-queue", e => e.ConfigureConsumer<UserSingInConsumer>(context));
+    });
+});
+
+
 services
     .AddInfrastructure(configuration)
     .AddInfrastructureAuth(configuration)
-    .AddInfrastructureNotifi();
+    .AddInfrastructureNotifi(configuration);
 
 services.AddApplication()
     .AddApplicationAuth()
