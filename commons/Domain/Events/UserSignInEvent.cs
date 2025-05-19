ed6571d6bad28161f25219ce04ef030159d4cf0b
Fabian Ramirez <fabian.ramirez@onoff.com.co>
2025-05-19 07:42:59 -0500

    Envents Integratiosn

---
+++ commons/Domain/Events/UserSignInEvent.cs
@@ -0,0 +1,8 @@
+﻿using Domain.Abstractions.Events;
+
+namespace Domain.Events;
+
+public record UserSignInEvent(Guid UserId, string Email) : IDomainEvent
+{
+    public DateTime OccurredOn { get; init; } = DateTime.UtcNow;
+}
---
+++ commons/Domain/Events/UserSignUpEvent.cs
@@ -0,0 +1,8 @@
+﻿using Domain.Abstractions.Events;
+
+namespace Domain.Events;
+
+public record UserSignUpEvent(Guid UserId, string Email) : IDomainEvent
+{
+    public DateTime OccurredOn { get; init; } = DateTime.UtcNow;
+}
