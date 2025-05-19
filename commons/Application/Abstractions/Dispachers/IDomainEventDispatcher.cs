59d1ce779953f18167d1a77cb34001ad852bc572
Fabian Ramirez <fabian.ramirez@onoff.com.co>
2025-05-19 07:42:48 -0500

    Include Mast Transit

---
+++ commons/Application/Abstractions/Dispachers/IDomainEventDispatcher.cs
@@ -0,0 +1,8 @@
+﻿using Domain.Abstractions.Events;
+
+namespace Application.Abstractions.Dispachers;
+
+public interface IDomainEventDispatcher
+{
+    Task DispatchAsync(IEnumerable<IDomainEvent> domainEvents);
+}
